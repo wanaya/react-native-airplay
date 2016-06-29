cc93c31bbbd15cb308245dae8495ecd9afc7bb17
guillermoncircle <memoanaya@gmail.com>
2016-06-29 18:36:39 -0500

    first commit

---
+++ Example/index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import RAirPlayButton from 'react-native-airplay'
+
+class Example extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RAirPlayButton style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#333333',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Example', () => Example);
